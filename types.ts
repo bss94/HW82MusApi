629749dcd6e0d9ca10e615405197292914b1118a
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-09-06 14:59:29 +0600

    step 8:HW83; complete models User TrackHistory, create method to user model:checkpassword generatetoken

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import { Model } from 'mongoose';
+
 export interface ArtistMutation {
   name: string;
   information: string;
@@ -16,3 +18,16 @@ export interface TrackMutation {
   album: string;
   time: string;
 }
+
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+}
+
+export interface UserMethods {
+  checkPassword(password: string): Promise<boolean>;
+  generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
