b28065291c877d35028849a0f642e357b70dc26f
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-09-06 15:24:59 +0600

    step 9:HW83: complete router users, create route post / create new user, post /session login user,create new token

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import config from './config';
 import artistsRouter from './routers/artists';
 import albumsRouter from './routers/albums';
 import tracksRouter from './routers/tracks';
+import usersRouter from './routers/users';
 
 const app = express();
 const port = 8000;
@@ -15,6 +16,8 @@ app.use(express.static('public'));
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+
+app.use('/users', usersRouter);
 
 
 const run = async () => {
